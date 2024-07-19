3830b63a7675230d99b4203e9e043ba417e9ac0d
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-07-19 21:43:54 +0530

    Till Lab009 is added

---
+++ src/ex_13072024/Lab007.java
@@ -0,0 +1,20 @@
+package ex_13072024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        // age?
+
+        //max age of the person in the world? -
+        byte age = 122; // age > 127 // less memory -> smart container - good practice
+        int age1 = 100; // wasting memory - 214768348488
+
+        //program store pi with only 2 decimals 3.14 not 3.1455666778899
+        float pi = 3.14f;
+        double pi_d = 3.145566778899;
+
+        // byte - datatype
+        // Byte - memory allocation
+
+
+    }
+}
---
+++ src/ex_13072024/Lab009.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+
+        //Formatting
+        int age = 98;
+        System.out.println("your age is -> " + age);
+        // %d -> any integer - byte, short,int or long
+        // %s -> String
+        // %f -> float
+        // %c -> char
+        System.out.printf("Your age is -> %d ",age);
+    }
+}
