9ecbcc1b4908effbcaee3ab5c91c0666f25c8945
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-07-22 10:20:55 +0530

    20 07 2024 is added

--- src/ex_14072024/Lab077.java
+++ src/ex_14072024/Lab077.java
@@ -1,6 +1,6 @@
 package ex_14072024;
 
-public class Lab77 {
+public class Lab077 {
     public static void main(String[] args) {
         String s1 = "Vinay";
         // Non primitive data types - String, Arrays, Class, Enum.. more will come
